00d095bc0007cd26ea2c2b3b1b59b08274cd9f1e
karolha <karol_ha@interia.pl>
2016-11-30 10:57:56 +0100

    directory refactoring

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,6 +5,7 @@
 
 #include <SystemClock_Config.h>
 #include <uart_usb.h>
+#include "rgb_pwm.h"
 
 
 int main(void)
@@ -13,13 +14,18 @@ int main(void)
 	SystemClock_Config();
 
 	uart_usb_init();
+	RGB_Pwm_Init();
 
 	char str[1024];
 	int n = 0;
 	while (1)
 	{
+		// Set led Duty Cycle in %
+		RGB_Set_PulseWidth(LED_R, 75);
 		snprintf(str, 1024, "%d\n", ++n);
-		uart_usb_send(str, strlen(str));
+		//uart_usb_send(str, strlen(str));
+		HAL_UART_Transmit(&uart_usb_handle, (uint8_t*)str, strlen(str), 1000);
+
 		HAL_Delay(500);
 	}
 }
