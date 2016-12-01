25a108670fee1ec58178d2eb7550db9cea992fbe
maciejgaleja <maciej.galeja@o2.pl>
2016-12-01 19:23:26 +0100

    RGB patterns and colors
    Added pattern and color management: color can be set as binary
    combination of RGB channels and intensity changes in time

--- firmware/inc/rgb_pwm.h
+++ firmware/inc/rgb_pwm.h
@@ -10,7 +10,10 @@
 
 #include "stm32f1xx.h"
 
-#define PWM_PERIOD 9999
+#define PWM_PERIOD 				9999
+#define RGB_PATTERN_MIN			5
+#define RGB_PATTERN_DELTA		10
+#define RGB_PATTERN_MAX			250
 
 extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim4;
@@ -26,5 +29,8 @@ void RGB_Timer3_Init();
 void RGB_Timer4_Init();
 void RGB_Pwm_Init();
 void RGB_Set_PulseWidth(LED_TypeDef led, uint8_t pulseWidth);
+void RGB_Set_Color(uint8_t R, uint8_t G, uint8_t B);
+void RGB_Set_Pattern_Color(uint8_t R, uint8_t G, uint8_t B);
+void RGB_Pattern_Update();
 
 #endif /* RGB_PWM_H_ */
