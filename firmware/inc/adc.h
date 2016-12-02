664d8755108bd2b389717caf1cae419064fc4d19
karolha <karol_ha@interia.pl>
2016-12-02 18:38:04 +0100

    ADC configured with DMA, 8 channels
    Problem with DMA fixed.

---
+++ firmware/inc/adc.h
@@ -0,0 +1,16 @@
+/*
+ * adc.h
+ *
+ *  Created on: 1 gru 2016
+ *      Author: Karollo
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+void ADC1_Init();
+void ADC1_Periph_Init();
+void ADC1_Gpio_Init();
+void ADC1_DMA_Init();
+
+#endif /* ADC_H_ */
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -7,6 +7,10 @@
 #include <uart_usb.h>
 #include "rgb_pwm.h"
 #include "debug.h"
+#include "adc.h"
+
+uint8_t flaga = 0;
+volatile uint32_t adc_value[8];
 
 int main(void)
 {
@@ -15,6 +19,7 @@ int main(void)
 
 	uart_usb_init();
 	RGB_Pwm_Init();
+	ADC1_Init();
 
 	int n = 0;
 	int pulse = 0;
@@ -36,10 +41,15 @@ int main(void)
 		if (n % 100 == 0)
 		{
 			++n;
-			debug("%d\n", n);
+			//debug("%d\n", n);
 		}
 		pulse += delta;
 		++n;
 		HAL_Delay(10);
+		//display adc values
+		if(flaga == 1){
+			debug("%lu	%lu	%lu	%lu	%lu	%lu	%lu	%lu\n", adc_value[0], adc_value[1], adc_value[2], adc_value[3], adc_value[4], adc_value[5], adc_value[6], adc_value[7]);
+			flaga = 0;
+		}
 	}
 }
