728e5b8d3de72128c1c2f6e46da7ef74accd630c
maciejgaleja <maciej.galeja@o2.pl>
2016-12-08 17:14:12 +0100

    Added error handling
    Added error handling functions

---
+++ firmware/inc/error_handler.h
@@ -0,0 +1,14 @@
+/*
+ * error_handler.h
+ *
+ *  Created on: 08.12.2016
+ *      Author: macie
+ */
+
+#ifndef ERROR_HANDLER_H_
+#define ERROR_HANDLER_H_
+
+void error_handler();
+void error_handler_str(const char *str);
+
+#endif /* ERROR_HANDLER_H_ */
